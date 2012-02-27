bef3865f00bf3dc799fca06b0266e21875fd945f
David de Boer <david@ddeboer.nl>
2012-02-27 11:01:17 +0100

    Update interface

--- DocumentInterface.php
+++ DocumentInterface.php
@@ -2,9 +2,81 @@
 
 namespace Ddeboer\DocumentManipulationBundle;
 
+/**
+ * A document
+ */
 interface DocumentInterface
 {
-    public function save($filename);
+    /**
+     * Save the document to a file
+     *
+     * @param string $filename  Filename to save to
+     * @param string $type      Type to save file as 
+     * @return DocumentInterface
+     */
+    function save($filename, $type = null);
 
-    public function concatenate()
+    /**
+     * (Mail) merge this document with data
+     *
+     * @param DocumentDataInterface $data   Data to merge into document
+     * @return DocumentInterface
+     */
+    function merge(DocumentDataInterface $data);
+
+    /**
+     * Append another document to this document
+     * 
+     * @return DocumentInterface 
+     */
+    function append(self $document);
+
+    /**
+     * Append multiple documents to this document
+     *
+     * @return DocumentInterface
+     */
+    function appendMultiple(array $documents);
+
+    /**
+     * Append this document to another document
+     *
+     * @return DocumentInterface
+     */
+    function appendTo(self $document);
+
+    /**
+     * Prepend another document to this document
+     *
+     * @return DocumentInterface
+     */
+    function prepend(self $document);
+
+    /**
+     * Prepend multiple documents to this document
+     */
+    function prependMultiple(array $documents);
+
+    /**
+     * Prepend this document to another document
+     *
+     * @return DocumentInterface
+     */
+    function prependTo(self $document);
+
+    /**
+     * Put this document in front of another document
+     *
+     * @return DocumentInterface $background
+     * @param DocumentInterface $document   The background document
+     */
+    function putInFront(self $background);
+
+    /**
+     * Put this document behind another document
+     *
+     * @param DocumentInterface $foreground
+     * @return DocumentInterface
+     */
+    function putBehind(self $foreground);
 }
